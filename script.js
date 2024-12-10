3406f370808120849f61838cc78d9f7d7125e1e6
sujal8791 <nadexsujal9@gmail.com>
2024-12-10 22:05:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const text = "I Love You Soo Much";
+let index = 0;
+
+function typeAnimation() {
+  const animatedText = document.getElementById("animatedText");
+  if (index < text.length) {
+    animatedText.innerHTML += text[index];
+    index++;
+    setTimeout(typeAnimation, 150);
+  }
+}
+
+window.onload = typeAnimation;
